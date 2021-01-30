050956fc918b377896e1df3becf102c4c7bd5661
AnastasiaIQA12 <Sinka8900@mail.ru>
2021-01-30 09:38:10 +0400

    Add Test for old functionality

--- src/main/java/ru/netology/DataGenerator.java
+++ src/main/java/ru/netology/DataGenerator.java
@@ -1,13 +1,22 @@
 package ru.netology;
 
+import com.github.javafaker.Faker;
+
+import java.time.LocalDate;
+import java.util.Locale;
+
 public class DataGenerator {
-    private DataGenerator(){}
+    private DataGenerator() {
+    }
 
-    public static class Registration{
-        private Registration(){}
-
-        public static RegistrationByCardInfo generateByCard(String locate){
-            Faker
+    public static class Registration {
+        private Registration() {
         }
+
+        public static RegistrationByCardInfo generateByCard() {
+            Faker faker = new Faker(new Locale("ru"));
+            return new RegistrationByCardInfo(faker.address().city(), LocalDate.now().plusDays(3), faker.name().fullName(), faker.phoneNumber().phoneNumber());
+        }
+
     }
 }
